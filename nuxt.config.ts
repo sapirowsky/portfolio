e53d60e598445a422b51de973e052e259bd0f9cc
sapirowsky <sapirowsky@gmail.com>
2022-08-21 01:41:30 +0200

    github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    target: 'static',
+    router: {
+        base: '/Portfolio/'
+    },
+
     css: ['@/assets/css/main.css'],
     postcss: {
         plugins: {

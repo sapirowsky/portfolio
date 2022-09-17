9e7f3f3cfa1f472f2ef8f509b50deefcb802ca05
sapirowsky <sapirowsky@gmail.com>
2022-09-17 20:52:29 +0200

    silly me #2

--- app.vue
+++ app.vue
@@ -8,16 +8,13 @@
     htmlAttrs: {
       lang: 'pl'
     },  
-    bodyAttrs: {
-        class: computed(() => darkMode.value ? 'bg-slate-900' : 'bg-white')
-      },
-      title: 'Sapyr Portfolio',
-      meta: [
-        {
-            hid: 'description',
-            name: 'description',
-            content: 'You can find sapyr\'s portfolio here!'
-        }
-      ],
+    title: 'Sapyr Portfolio',
+    meta: [
+    {
+        hid: 'description',
+        name: 'description',
+        content: 'You can find sapyr\'s portfolio here!'
+    }
+    ],
   })
 </script>

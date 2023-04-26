8ec1fdd471a30cdb99e0d0da32e4722b77b6ff8a
sapirowsky <sapirowsky@gmail.com>
2023-04-26 21:33:20 +0200

    added more projects to my work deleted unused css

--- components/MyWork.vue
+++ components/MyWork.vue
@@ -4,16 +4,17 @@
       <UiDot class="text-green-400" />
       <h1 class="text-gray-400">Some of my Work</h1>
     </div>
-    <div class="w-full flex flex-col items-center justify-center gap-1 mt-2">
+    <div class="w-full flex flex-col items-center justify-center gap-8 mt-2">
       <div class="container transition-all duration-150 ease-in-out">
         <div
           class="z-20 text-right flex flex-col justify-around col-start-1 col-end-[-1] row-end-[-1] md:col-start-5 md:col-end-13 md:row-end-1 p-4 md:p-0"
         >
           <h1 class="text-xl font-bold">World Cup Info</h1>
           <p class="w-full md:bg-darkBg p-4">
-            This project shows 2022 world cup info and i created it with Natalia
-            K. as a school project. Actually only things that i didn't create
-            here was logo and front page bracket.
+            For this project i had a little help from Natalia K. who created
+            logo and front page bracket for me. What's worth noticing is that,
+            the site have its own database and api. Also i implented skeleton
+            loading to this site.
           </p>
           <div class="text-gray-400 flex gap-2 justify-end flex-wrap">
             <p>Nuxt 3</p>
@@ -48,7 +49,7 @@
           <NuxtImg
             preset="highq"
             loading="lazy"
-            class="object-cover opacity-20 md:opacity-100"
+            class="w-full h-full object-cover opacity-20 md:opacity-100"
             src="/img/fifa.png"
             alt="picture of fifa site"
             height="400"
@@ -56,15 +57,104 @@
           />
         </NuxtLink>
       </div>
-      <div class="container"></div>
-      <div class="container"></div>
+
+      <div class="container transition-all duration-150 ease-in-out">
+        <div
+          class="z-20 text-left flex flex-col justify-around col-start-1 col-end-[-1] row-end-[-1] md:col-start-1 md:col-end-8 md:row-end-1 p-4 md:p-0"
+        >
+          <h1 class="text-xl font-bold">Instaling Word DB</h1>
+          <p class="w-full md:bg-darkBg p-4">
+            This project was made to solve problem of remembering Deutsch words,
+            also to make mine and my class life a little easier. For this
+            project i had to make a simple db & rest api.
+          </p>
+          <div class="text-gray-400 flex gap-2 justify-start flex-wrap">
+            <p>Nuxt 3</p>
+            <p>Prisma</p>
+            <p>Tailwind</p>
+            <p>SQL</p>
+            <p>REST</p>
+          </div>
+          <div class="flex justify-start gap-4 mt-2">
+            <NuxtLink
+              to="https://github.com/sapirowsky/instalingWordDB"
+              aria-label="GitHub"
+              target="_b"
+            >
+              <UiGitHub class="w-8 h-8 md:w-12 md:h-12" />
+            </NuxtLink>
+            <NuxtLink
+              to="https://instaling.kamilkruszona.dev/"
+              aria-label="instaling"
+              target="_b"
+            >
+              <UiExternalLink class="w-8 h-8 md:w-12 md:h-12" />
+            </NuxtLink>
+          </div>
+        </div>
+        <NuxtLink
+          class="col-start-1 col-end-[-1] row-end-[-1] md:col-start-5 md:col-end-13 md:row-end-1 blur-sm hover:blur-0"
+          to="https://instaling.kamilkruszona.dev/"
+          target="_b"
+        >
+          <NuxtImg
+            preset="highq"
+            loading="lazy"
+            class="w-full h-full object-cover opacity-20 md:opacity-100"
+            src="/img/instaling.png"
+            alt="picture of fifa site"
+            height="400"
+            width="800"
+          />
+        </NuxtLink>
+      </div>
+
+      <div class="container transition-all duration-150 ease-in-out">
+        <div
+          class="z-20 text-right flex flex-col justify-around col-start-1 col-end-[-1] row-end-[-1] md:col-start-5 md:col-end-13 md:row-end-1 p-4 md:p-0"
+        >
+          <h1 class="text-xl font-bold">Terminal</h1>
+          <p class="w-full md:bg-darkBg p-4">
+            Fun project made to imitate Windows PowerShell terminal. But as you
+            may guess, it doesn't have to many features.
+          </p>
+          <div class="text-gray-400 flex gap-2 justify-end flex-wrap">
+            <p>Nuxt 3</p>
+            <p>Tailwind</p>
+          </div>
+          <div class="flex justify-end gap-4 mt-2">
+            <NuxtLink
+              to="https://github.com/sapirowsky/browserTerminal"
+              aria-label="GitHub"
+              target="_b"
+            >
+              <UiGitHub class="w-8 h-8 md:w-12 md:h-12" />
+            </NuxtLink>
+            <NuxtLink
+              to="https://terminal.kamilkruszona.dev/"
+              aria-label="terminal"
+              target="_b"
+            >
+              <UiExternalLink class="w-8 h-8 md:w-12 md:h-12" />
+            </NuxtLink>
+          </div>
+        </div>
+        <NuxtLink
+          class="col-start-1 col-end-[-1] row-end-[-1] md:col-start-1 md:col-end-8 md:row-end-1 blur-sm hover:blur-0"
+          to="https://terminal.kamilkruszona.dev/"
+          target="_b"
+        >
+          <NuxtImg
+            preset="highq"
+            loading="lazy"
+            class="w-full h-full object-cover opacity-20 md:opacity-100"
+            src="/img/terminal.png"
+            alt="picture of fifa site"
+            height="400"
+            width="800"
+          />
+        </NuxtLink>
+      </div>
     </div>
   </section>
 </template>
-
-<style>
-.container {
-  max-width: 1400px;
-  @apply grid grid-cols-12;
-}
-</style>
